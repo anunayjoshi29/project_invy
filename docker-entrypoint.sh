3e2ad0e894b84f2811cb69f7aac987e2643507ba
anunayjoshi29 <ajoshi@bt.iitr.ac.in>
2023-08-15 06:27:31 +0530

    final fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,5 @@ sleep 5
 bin/rails db:create
 rails generate after_party:install
 rake db:migrate
-bin/rails after_party run
-# sleep 5000
+bin/rails after_party:run
 bundle exec rails s -p 3003 -b 0.0.0.0
